7b63fcbcacfa5a274086ef2120dfc152ba0c0f1f
huulamnguyen <liamnguyen.swe@gmail.com>
2021-12-21 19:19:15 -0500

    commit tree e1fc9e1d9d68473cd0a1738da913d38774f7ea82

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject);
+
+function submitData(userName, userEmail){
+
+    const userData = {
+        name: userName,
+        email: userEmail
+    }
+    
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(userData),
+    }
+
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(response => response.json())
+    .then(userData => document.querySelector('body').append(document.createElement('p').innerText = userData.id))
+    .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        document.querySelector('body').append(document.createElement('p').innerText = error.message)
+        console.log(error.message);
+    });
+}
